89d546c67836d697b7567a4546a5112651409b17
Sef <39380372+S-E-F@users.noreply.github.com>
2023-12-06 11:14:32 +0100

    Remove newline when piping stdout

--- base64/Program.cs
+++ base64/Program.cs
@@ -6,7 +6,7 @@ if (!Console.IsInputRedirected)
     return 1;
 }
 
-if (args.Length is not 1 || args[0] is "help" or "?" or "--help" or "-h")
+if (args.Length is not 1 || args[0] is not ("encode" or "decode"))
 {
     const string blue = "\x1b[34m";
     const string fgReset = "\x1b[39m";
@@ -27,16 +27,18 @@ if (args.Length is not 1 || args[0] is "help" or "?" or "--help" or "-h")
 
 var input = await Console.In.ReadToEndAsync();
 
+Func<string, Task> stdout = Console.IsOutputRedirected ? Console.Out.WriteAsync : Console.Out.WriteLineAsync;
+
 if (args[0] is "encode")
 {
     var bytes = Encoding.UTF8.GetBytes(input);
-    await Console.Out.WriteLineAsync(Convert.ToBase64String(bytes));
+    await stdout(Convert.ToBase64String(bytes));
 }
 
 else if (args[0] is "decode")
 {
     var bytes = Convert.FromBase64String(input);
-    await Console.Out.WriteLineAsync(Encoding.UTF8.GetString(bytes));
+    await stdout(Encoding.UTF8.GetString(bytes));
 }
 
 return 0;
